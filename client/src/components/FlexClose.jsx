c633ff69b5268a2f762ada751a391ae47758ee99
abassj1 <abass-jam@hotmail.com>
2023-02-08 10:54:59 +0000

    Added more components to improve cutomization for the social widgets

--- client/src/components/FlexClose.jsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-import { styled } from "@mui/system";
-
-const FlexCol = styled(Box)({
-    display: "flex",
-    alignItems: "center",
-});
-
-export default FlexCol;
